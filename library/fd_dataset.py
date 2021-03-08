28b4eced6fa4e912ee3b8cda8c9a5732d7269e4d
kbyshiro <45071451+kbyshiro@users.noreply.github.com>
2021-03-08 17:36:23 +0900

    Update fd_dataset.py
    create Label

--- library/fd_dataset.py
+++ library/fd_dataset.py
@@ -21,6 +21,19 @@ class DataSet:
         return self.X[index], self.t[index]
     
 
+class DataSet:
+    def __init__(self, X=[], t=[]):
+        self.X = X # 入力
+        self.t = t # 出力
+
+    def __len__(self):
+        return len(self.X) 
+
+    def __getitem__(self, index):
+        # index番目の入出力ペアを返す
+        return self.X[index], self.t[index]
+    
+
 class FD_Dataset():
   def __init__(self, dataset_path="/content/drive/My Drive/CMAPSSData"):
     self._dataset_path = dataset_path
@@ -30,11 +43,20 @@ class FD_Dataset():
     op_settings_columns = ["Op_Setting_"+str(i) for i in range(1,4)]
     sensor_columns =["Sensor_"+str(i) for i in range(1,22)]
     self.column_names = index_columns_names + op_settings_columns + sensor_columns
-    self.target_name = 'Target_Remaining_Useful_Life'
+    self.target_name = 'Target_RUL'
+    self.label_name = 'Label'
     self.train = DataSet()
     self.test = DataSet()
 
-  def load(self, data_id, drop_const = True, rul=True):
+
+  def load(self, data_id=1):
+    '''
+    やってること：
+    train, test, rul.txt の読み込み
+    Noneの値を落とす
+    カラムの名前付け
+    '''
+
     self._train_name = 'train_FD00{}.txt'.format(data_id)
     self._test_name = 'test_FD00{}.txt'.format(data_id)
     self._test_rul_name = 'RUL_FD00{}.txt'.format(data_id)
@@ -50,19 +72,18 @@ class FD_Dataset():
     self.raw_train.columns = self.column_names
     self.raw_test.columns = self.column_names
     
-    if rul: 
-      self.GetTrainRUL(self.raw_train)
-      self.GetTestRUL(self.raw_test, self.raw_rul_test)
-
-    if drop_const:
-      self.DropConst()
-
-  def GetTrainRUL(self, raw_train):
-      max_cycle = raw_train.groupby('UnitNumber')['Cycle'].max().reset_index()
+  def CreateRUL(self):
+      '''
+      Train：CycleのデータからRULをせいせい
+      Test:Cycleのデータとrul.txtの値からRULを生成
+      rul.txtはTestUnitのサイクルが終了した時点でのRULの値を示している。
+      '''
+      # Train
+      max_cycle = self.raw_train.groupby('UnitNumber')['Cycle'].max().reset_index()
       max_cycle.columns = ['UnitNumber', 'MaxOfCycle']
       
       # merge the max cycle back into the original frame
-      raw_train_merged = raw_train.merge(max_cycle, left_on='UnitNumber', right_on='UnitNumber', how='inner')
+      raw_train_merged = self.raw_train.merge(max_cycle, on='UnitNumber', how='inner')
       
       # calculate RUL for each row
       Target_Remaining_Useful_Life = raw_train_merged["MaxOfCycle"] - raw_train_merged["Cycle"]
@@ -71,63 +92,124 @@ class FD_Dataset():
       self.train.X = raw_train_merged.drop("MaxOfCycle", axis=1)
       self.train.t = pd.Series(Target_Remaining_Useful_Life, name=self.target_name)
 
-
-  def GetTestRUL(self, raw_test, raw_rul_test):
-      raw_rul_test['UnitNumber'] = raw_rul_test.index+1
-      raw_rul_test.rename(columns={0:"RUL"}, inplace=True)
-      max_cycle = raw_test.groupby('UnitNumber')['Cycle'].max().reset_index()
+      #Test
+      self.raw_rul_test['UnitNumber'] = self.raw_rul_test.index+1
+      self.raw_rul_test.rename(columns={0:"ActualRUL"}, inplace=True)
+      max_cycle = self.raw_test.groupby('UnitNumber')['Cycle'].max().reset_index()
       max_cycle.columns = ['UnitNumber', 'MaxOfCycle']
 
-      raw_rul_test["RUL_failed"] = raw_rul_test['RUL']+max_cycle["MaxOfCycle"]
-      raw_rul_test.drop("RUL", axis=1, inplace=True)
-      raw_test = raw_test.merge(raw_rul_test,on='UnitNumber',how='left')
-      Target_Remaining_Useful_Life = raw_test['RUL_failed']-raw_test['Cycle']
+      self.raw_rul_test["MaxRUL"] = self.raw_rul_test['ActualRUL']+max_cycle["MaxOfCycle"]
+      self.raw_rul_test.drop("ActualRUL", axis=1, inplace=True)
+      raw_test_merged = self.raw_test.merge(self.raw_rul_test,on='UnitNumber',how='left')
+      Target_Remaining_Useful_Life = raw_test_merged['MaxRUL']-raw_test_merged['Cycle']
       
       # set DataSet instance
-      self.test.X = raw_test.drop("RUL_failed", axis=1)
+      self.test.X = raw_test_merged.drop("MaxRUL", axis=1)
       self.test.t = pd.Series(Target_Remaining_Useful_Life, name = self.target_name)
   
+  def InsertFailure(self, x, target_alarm):
+      last = x.index[-1]
+      x.iloc[-target_alarm:] = 1
+      return x
+  
+  def CreateLabel(self, target_alarm):
+      '''
+      Cycleの値からLabelの生成
+      '''
+      all_data = self.raw_train
+      all_data['Label'] = 0
+      all_data['Label'] = all_data.groupby('UnitNumber')['Label'].transform(self.InsertFailure, target_alarm=target_alarm)
+
+      #split test data
+      random.seed(1234)
+      idx = random.sample(range(1, 101), 20)
+      rest_idx = [i for i in range(1, 101) if i not in idx]
+      test = all_data[all_data['UnitNumber'].isin(idx)]
+      train = all_data[all_data['UnitNumber'].isin(rest_idx)]
+
+      # Dataset instance 
+      self.train.X = train.iloc[:, :-1]
+      self.train.t = train.iloc[:, -1]
+
+      self.test.X = test.iloc[:, :-1]
+      self.test.t = test.iloc[:, -1]
+      return self.train
+
   def DropConst(self):
+      '''
+      変化のないカラムとオプションを落とす
+      '''
       leakage_to_drop = ['Cycle', 'Op_Setting_1', 'Op_Setting_2', 'Op_Setting_3']
       leakage_to_drop += ['Sensor_'+str(i) for i in [1, 5, 6, 10, 16, 18, 19]]
       self.train.X.drop(leakage_to_drop, axis = 1, inplace=True)
       self.test.X.drop(leakage_to_drop, axis = 1, inplace=True)
 
   def MinMaxNorm(self):
-    tmp = pd.concat([self.train.X.iloc[:, 1:], self.test.X.iloc[:, 1:]]).reset_index(drop=True)
-    train_size = len(self.train)
-    X_min, X_max = tmp.min(), tmp.max()
-    tmp = (tmp-X_min)/(X_max-X_min)
-    self.train.X.iloc[:, 1:] = tmp.iloc[:train_size, :]
-    self.test.X.iloc[:, 1:] = tmp.iloc[train_size:, :].reset_index(drop=True)
+      tmp = pd.concat([self.train.X.iloc[:, 1:], self.test.X.iloc[:, 1:]]).reset_index(drop=True)
+      train_size = len(self.train)
+      X_min, X_max = tmp.min(), tmp.max()
+      tmp = (tmp-X_min)/(X_max-X_min)
+      self.train.X.iloc[:, 1:] = tmp.iloc[:train_size, :]
+      self.test.X.iloc[:, 1:] = tmp.iloc[train_size:, :].reset_index(drop=True)
 
   def LimitRUL(self, RUL_limit):
     self.train.t = pd.Series(np.minimum(self.train.t, RUL_limit), name=self.target_name)
     self.test.t = pd.Series(np.minimum(self.test.t, RUL_limit), name=self.target_name)
 
-  def SlideWindow(self, dataset, ws, cs=1, mode='LSTMCNN'):
+  def SlideWindow(self, dataset, ws, cs=1, model='DAG'):
     #X.size= (n-ws, cs, ws/cs, n_feautures)
     combined_dataset = pd.concat([dataset.X, dataset.t], axis=1)
     unit_max = combined_dataset['UnitNumber'].max()
-    width = int(ws/cs)
+    width = ws//cs
     X, y = [], []
-    if mode == 'LSTMCNN':
+    if model == 'DAG':
       for k in range(unit_max):
         tmp_data = combined_dataset[combined_dataset['UnitNumber']==k+1].drop('UnitNumber', axis=1).values
         n = len(tmp_data)
         for i in range(n-ws):
             X.append([tmp_data[i+(j)*width:i+(j+1)*width, :-1] for j in range(cs)])
             y.append(tmp_data[i+ws, -1])
-    elif mode == 'LSTM':
+    elif model == 'LSTM':
       for k in range(unit_max):
         tmp_data = combined_dataset[combined_dataset['UnitNumber']==k+1].drop('UnitNumber', axis=1).values
         n = len(tmp_data)
         for i in range(n-ws):
             X.append(tmp_data[i:i+width, :-1])
             y.append(tmp_data[i+ws, -1])
+    else: 
+      print('Invalid Mode name')
     dataset.X = torch.tensor(X).float()
     dataset.t = torch.tensor(y).float()
     return dataset
+
+  def GetRULDataset(self, ws, cs, batch_size, data_id=1, limit_flag = False, limit_value = None, model='DAG'):
+    self.load(data_id)
+    self.CreateRUL()
+    self.DropConst()
+    self.MinMaxNorm()
+
+    if limit_flag: 
+      self.LimitRUL(limit_value)
+
+    self.train = self.SlideWindow(self.train, ws, cs, model)
+    self.test = self.SlideWindow(self.test, ws, cs, model)
+
+    train_dataloader = torch.utils.data.DataLoader(self.train, batch_size= batch_size)
+    test_dataloader = torch.utils.data.DataLoader(self.test, batch_size= 1)
+    return train_dataloader, test_dataloader
+
+  def GetLabelDataset(self, ws, cs, batch_size, data_id=1, target_alarm = 20, model='DAG'):
+    self.load(data_id)
+    self.CreateLabel(target_alarm)
+    self.DropConst()
+    self.MinMaxNorm()
+
+    self.train = self.SlideWindow(self.train, ws, cs, model)
+    self.test = self.SlideWindow(self.test, ws, cs, model)
+
+    train_dataloader = torch.utils.data.DataLoader(self.train, batch_size= batch_size)
+    test_dataloader = torch.utils.data.DataLoader(self.test, batch_size= 1)
+    return train_dataloader, test_dataloader
 
 if __name__ == '__main__':
   fd = FD_Dataset()
