c54327f329a5577dc566961c60494deb661612f5
kbyshiro <45071451+kbyshiro@users.noreply.github.com>
2021-03-16 04:24:54 +0900

    Update fd_dataset.py
    dataframe基準の操作に置き換え。
    sliding windowをgroupbyですっきりさせた。

--- library/fd_dataset.py
+++ library/fd_dataset.py
@@ -87,11 +87,10 @@ class FD_Dataset():
       raw_train_merged = self.raw_train.merge(max_cycle, on='UnitNumber', how='inner')
       
       # calculate RUL for each row
-      Target_Remaining_Useful_Life = raw_train_merged["MaxOfCycle"] - raw_train_merged["Cycle"]
+      raw_train_merged[self.target_name] = raw_train_merged["MaxOfCycle"] - raw_train_merged["Cycle"]
       
       # set DataSet instance
-      self.train.X = raw_train_merged.drop("MaxOfCycle", axis=1)
-      self.train.t = pd.Series(Target_Remaining_Useful_Life, name=self.target_name)
+      self.train_dataframe = raw_train_merged.drop('MaxOfCycle', axis=1)
 
       #Test
       self.raw_rul_test['UnitNumber'] = self.raw_rul_test.index+1
@@ -102,11 +101,10 @@ class FD_Dataset():
       self.raw_rul_test["MaxRUL"] = self.raw_rul_test['ActualRUL']+max_cycle["MaxOfCycle"]
       self.raw_rul_test.drop("ActualRUL", axis=1, inplace=True)
       raw_test_merged = self.raw_test.merge(self.raw_rul_test,on='UnitNumber',how='left')
-      Target_Remaining_Useful_Life = raw_test_merged['MaxRUL']-raw_test_merged['Cycle']
+      raw_test_merged[self.target_name] = raw_test_merged['MaxRUL']-raw_test_merged['Cycle']
       
       # set DataSet instance
-      self.test.X = raw_test_merged.drop("MaxRUL", axis=1)
-      self.test.t = pd.Series(Target_Remaining_Useful_Life, name = self.target_name)
+      self.test_dataframe = raw_test_merged.drop('MaxRUL', axis=1)
   
   def InsertFailure(self, x, target_alarm):
       last = x.index[-1]
@@ -123,18 +121,12 @@ class FD_Dataset():
 
       #split test data
       random.seed(1234)
-      idx = random.sample(range(1, 101), 20)
+      idx = random.sample(range(1, 101), 20) #idx for test data
       rest_idx = [i for i in range(1, 101) if i not in idx]
-      test = all_data[all_data['UnitNumber'].isin(idx)].reset_index(drop=True)
-      train = all_data[all_data['UnitNumber'].isin(rest_idx)].reset_index(drop=True)
+      self.test_dataframe = all_data[all_data['UnitNumber'].isin(idx)].reset_index(drop=True)
+      self.train_dataframe = all_data[all_data['UnitNumber'].isin(rest_idx)].reset_index(drop=True)
 
-      # Dataset instance 
-      self.train.X = train.iloc[:, :-1]
-      self.train.t = train.iloc[:, -1]
-
-      self.test.X = test.iloc[:, :-1]
-      self.test.t = test.iloc[:, -1]
-      return self.train
+      return 
 
   def DropConst(self):
       '''
@@ -142,48 +134,48 @@ class FD_Dataset():
       '''
       leakage_to_drop = ['Cycle', 'Op_Setting_1', 'Op_Setting_2', 'Op_Setting_3']
       leakage_to_drop += ['Sensor_'+str(i) for i in [1, 5, 6, 10, 16, 18, 19]]
-      self.train.X.drop(leakage_to_drop, axis = 1, inplace=True)
-      self.test.X.drop(leakage_to_drop, axis = 1, inplace=True)
+      self.train_dataframe.drop(leakage_to_drop, axis = 1, inplace=True)
+      self.test_dataframe.drop(leakage_to_drop, axis = 1, inplace=True)
 
   def MinMaxNorm(self):
-      tmp = pd.concat([self.train.X.iloc[:, 1:], self.test.X.iloc[:, 1:]]).reset_index(drop=True)
-      train_size = len(self.train)
-      X_min, X_max = tmp.min(), tmp.max()
-      tmp = (tmp-X_min)/(X_max-X_min)
-      self.train.X.iloc[:, 1:] = tmp.iloc[:train_size, :]
-      self.test.X.iloc[:, 1:] = tmp.iloc[train_size:, :].reset_index(drop=True)
+      X = pd.concat([self.train_dataframe.iloc[:, 1:-1], self.test_dataframe.iloc[:, 1:-1]]).reset_index(drop=True)
+      train_size = self.train_dataframe.shape[0]
+      
+      X_min, X_max = X.min(), X.max()
+      X = (X-X_min)/(X_max-X_min)
+      self.train_dataframe.iloc[:, 1:-1] = X.iloc[:train_size, :]
+      self.test_dataframe.iloc[:, 1:-1] = X.iloc[train_size:, :].reset_index(drop=True)
 
   def LimitRUL(self, RUL_limit):
-    self.train.t = pd.Series(np.minimum(self.train.t, RUL_limit), name=self.target_name)
-    self.test.t = pd.Series(np.minimum(self.test.t, RUL_limit), name=self.target_name)
+    self.train_dataframe[self.target_name] = self.train_dataframe[self.target_name].transform(lambda x: np.minimum(x, RUL_limit))
+    self.test_dataframe[self.target_name] = self.test_dataframe[self.target_name].transform(lambda x: np.minimum(x, RUL_limit))
 
-  def SlideWindow(self, dataset, ws, cs=1, model='DAG'):
-    #X.size= (n-ws, cs, ws/cs, n_feautures)
-    combined_dataset = pd.concat([dataset.X, dataset.t], axis=1)
-    unit_max = combined_dataset['UnitNumber'].max()
+  def SlideWindow(self, dataframe, ws, cs=1, model_name='DAG'):
+    '''
+    input: dataframe
+    output: dataset obj
+    X.size= (n-ws, cs, ws//cs, n_feautures)
+    '''
+    dataset = DataSet()
+    X, t = [], []
     width = ws//cs
-    X, y = [], []
-    if model == 'DAG':
-      for k in range(unit_max):
-        tmp_data = combined_dataset[combined_dataset['UnitNumber']==k+1].drop('UnitNumber', axis=1).values
-        n = len(tmp_data)
-        for i in range(n-ws):
-            X.append([tmp_data[i+(j)*width:i+(j+1)*width, :-1] for j in range(cs)])
-            y.append(tmp_data[i+ws, -1])
-    elif model == 'LSTM':
-      for k in range(unit_max):
-        tmp_data = combined_dataset[combined_dataset['UnitNumber']==k+1].drop('UnitNumber', axis=1).values
-        n = len(tmp_data)
-        for i in range(n-ws):
-            X.append(tmp_data[i:i+width, :-1])
-            y.append(tmp_data[i+ws, -1])
-    else: 
-      print('Invalid Mode name')
-    dataset.X = torch.tensor(X).float()
-    dataset.t = torch.tensor(y).float()
+    groups = dataframe.groupby('UnitNumber')
+    for _, x in groups:
+      n = len(x)
+      x = torch.tensor(x.drop('UnitNumber', axis=1).values).float()
+      for i in range(n-ws):
+          X.append(x[i:i+ws, :-1])
+          t.append(x[i+ws, -1])
+    X = torch.stack(X, dim=0).float()
+    t = torch.stack(t, dim=0).float()
+
+    if model_name == 'DAG':
+      X = X.reshape(X.shape[0], cs, width, X.shape[-1])
+    dataset.X = X
+    dataset.t = t
     return dataset
 
-  def GetRULDataset(self, ws, cs, batch_size, data_id=1, limit_flag = False, limit_value = None, model='DAG'):
+  def GetRULDataset(self, ws, cs=1, batch_size, data_id=1, limit_flag = False, limit_value = None, model_name='DAG'):
     self.load(data_id)
     self.CreateRUL()
     self.DropConst()
@@ -192,21 +184,21 @@ class FD_Dataset():
     if limit_flag: 
       self.LimitRUL(limit_value)
 
-    self.train = self.SlideWindow(self.train, ws, cs, model)
-    self.test = self.SlideWindow(self.test, ws, cs, model)
+    self.train = self.SlideWindow(self.train_dataframe, ws, cs, model_name)
+    self.test = self.SlideWindow(self.test_dataframe, ws, cs, model_name)
 
     train_dataloader = torch.utils.data.DataLoader(self.train, batch_size= batch_size)
     test_dataloader = torch.utils.data.DataLoader(self.test, batch_size= 1)
     return train_dataloader, test_dataloader
 
-  def GetLabelDataset(self, ws, cs, batch_size, data_id=1, target_alarm = 20, model='DAG'):
+  def GetLabelDataset(self, ws, cs=1, batch_size, data_id=1, target_alarm = 20, model_name='DAG'):
     self.load(data_id)
     self.CreateLabel(target_alarm)
     self.DropConst()
     self.MinMaxNorm()
 
-    self.train = self.SlideWindow(self.train, ws, cs, model)
-    self.test = self.SlideWindow(self.test, ws, cs, model)
+    self.train = self.SlideWindow(self.train_dataframe, ws, cs, model_name)
+    self.test = self.SlideWindow(self.test_dataframe, ws, cs, model_name)
 
     train_dataloader = torch.utils.data.DataLoader(self.train, batch_size= batch_size)
     test_dataloader = torch.utils.data.DataLoader(self.test, batch_size= 1)
